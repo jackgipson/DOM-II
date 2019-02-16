a0c63d5f6630a7500b1d99ff697ba8407f496228
jackgipson <jackjgipson@gmail.com>
2019-02-16 12:49:33 -0800

    changed mouseover

--- js/index.js
+++ js/index.js
@@ -15,10 +15,22 @@
 // Mouseover
 
 const logoHeader = document.querySelector("h1");
+console.log(logoHeader);
+
 logoHeader.addEventListener('mouseover', function(event) {
     document.querySelector("h1").style.color = "red";
   //event.stopPropagation();
 })
+
+// Mouseover with Mouseout
+
+
+logoHeader.addEventListener('mouseout', function(event) {
+    document.querySelector("h1").style.color = "black";
+  //event.stopPropagation();
+})
+
+
 
 // Keydown
 
@@ -30,6 +42,8 @@ keysdown.addEventListener('keydown', function(event) {
 // Wheel
 
 const wheels = document.querySelector('.intro h2');
+console.log(wheels);
+
 wheels.addEventListener('wheel', function(event) {
   console.log(`This is a wheel`)
 })
@@ -44,56 +58,69 @@ var script = document.createElement("script");
   script.async = true;
   document.getElementsByTagName("script")[0].parentNode.appendChild(script);
 
-// Focus
+// // Focus
 
-  a:focus, a:active {
-    color: green;
-  }
+//   a:focus, a:active {
+//     color: green;
+//   }
   
-  <a id="myAnchor" href="https://www.w3schools.com">Visit W3Schools.com</a>
+//   <a id="myAnchor" href="https://www.w3schools.com">Visit W3Schools.com</a>
   
-  <p>Click the buttons to give focus and/or remove focus from the link above.</p>
+//   <p>Click the buttons to give focus and/or remove focus from the link above.</p>
   
- // <input type="button" onclick="getfocus()" value="Get focus">
-  //<input type="button" onclick="losefocus()" value="Lose focus">
+//  // <input type="button" onclick="getfocus()" value="Get focus">
+//   //<input type="button" onclick="losefocus()" value="Lose focus">
   
 
-  function getfocus() {
-    document.getElementById("myAnchor").focus();
-  }
+//   function getfocus() {
+//     document.getElementById("myAnchor").focus();
+//   }
   
-  function losefocus() {
-    document.getElementById("myAnchor").blur();
-  }
+//   function losefocus() {
+//     document.getElementById("myAnchor").blur();
+//   }
 
-// Resize
+// // Resize
 
-  function quarter() {
-    window.resizeTo(
-      window.screen.availWidth,
-      window.screen.availHeight
-    );
-  }
-console.log('What?');
+//   function quarter() {
+//     window.resizeTo(
+//       window.screen.availWidth,
+//       window.screen.availHeight
+//     );
+//   }
+// console.log('What?');
 
-// Scroll
+// // Scroll
 
-window.addEventListener('scroll', () => {
-    document.body.style.color = "brown";
-    });
+// const upDown = document.querySelector('.home');
+// upDown.addEventListener('wheel', function(event) {
+// event.currentTarget.style.color = "brown";
+//   console.log('srolling up and down');
 
-// Select
+// // Select
 
-selects.addEventListener('select', function(event) {
+// selects.addEventListener('select', function(event) {
     
-  })
+//   })
 
-  //Dblclick
+//   //Dblclick
 
-  const buttons = document.querySelectorAll(".btn");
-buttons.forEach(content => {
-  content.addEventListener('dblclick', function(event) {
-  console.log("select all");
-  event.target.style.color = "blue";
-  })
-})
+//   const buttons = document.querySelectorAll(".btn");
+// buttons.forEach(content => {
+//   content.addEventListener('dblclick', function(event) {
+//   console.log("select all");
+//   event.target.style.color = "blue";
+//   })
+// })
+
+// Testing out different things
+
+//let aTags = document.querySelectorAll("a");
+// let i;
+// for (i = 0; i < aTags.length; i++) {
+//   aTags[i].style.color = "red";
+// }
+
+// aTags.forEach(element => {
+//   element.style.color = 'blue';
+// });
